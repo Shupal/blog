75391597a15cb391a6de5b63570e342885cb1241
Shupal <stefan.andreas.hoefler@gmail.com>
2019-08-21 21:19:22 +0200

    Add markdown template processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
